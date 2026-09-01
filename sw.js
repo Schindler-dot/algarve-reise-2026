aecb6b9233df692c99089a68afb8102314f0710f
Schindler-dot <Schindler@ska-graz.com>
2026-09-01 14:17:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,12 @@
-const CACHE='algarve-reise-v12';
+const CACHE='algarve-reise-v13';
 const STATIC=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(Promise.all([
-    self.clients.claim(),
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
-    )
-  ]));
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  // HTML/navigation: always try the newest version first.
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request, {cache:'no-store'})
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put('./index.html', copy)).catch(()=>{});
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  // Other resources: network first, cache fallback.
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));});
+self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]));});
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET')return;
+ if(event.request.mode==='navigate'){
+   event.respondWith(fetch(event.request,{cache:'no-store'}).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy)).catch(()=>{});return resp;}).catch(()=>caches.match('./index.html')));
+   return;
+ }
+ event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return resp;})));
 });
